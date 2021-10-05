44e5f0c2cf8152b550a3542884d25ae98d7e5100
NickKir38 <87764064+NickKir38@users.noreply.github.com>
2021-10-05 09:48:40 -0400

    Update AllWetter.py

--- Code/AllWetter.py
+++ Code/AllWetter.py
@@ -151,7 +151,8 @@ def risk_parity_weights(w, covar_matr, w_t, constrtns):
     elif constrtns == 2:
         cons = ({'type': 'ineq', 'fun': lambda w: w})
     elif constrtns == 3: 
-        cons = ({'type': 'eq', 'fun': lambda w: np.sum(w)-1}, {'type': 'ineq', 'fun': lambda w: w})
+        cons = ({'type': 'eq', 'fun': lambda w: np.sum(w)-1}, 
+                {'type': 'ineq', 'fun': lambda w: w})
     #Do the optimization to get the optimal weights by minimizing error
     res = minimize(risk_parity_error, w, args=(covar_matr,w_t), method='SLSQP',constraints=cons, options={'disp': True})
     w_opt = np.asarray(res.x)
